b2be73f25e631d9a98145fca42231487fd9b38a9
timgo-1 <timochars@gmail.com>
2021-05-13 09:57:00 +0300

    Timz-Foodie-App

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+//import 'Core from core';
+import Vue from 'vue';
+import './plugins/vuetify';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+const app = new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+}).$mount('#app');
+if (window.Cypress) {
+    window.app = app;
+}
+
